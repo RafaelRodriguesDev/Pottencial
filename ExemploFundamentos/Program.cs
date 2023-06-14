6836915356a4f4c61ea790c7d258600cd5916915
Rafael Rodrigues <rafinhaa.rodriguees@gmail.com>
2023-06-14 15:23:12 -0300

    Modulo Concluido Sintaxe e Tipos de Dados em

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome= "Rafael";
+pessoa1.Idade = 28;
+pessoa1.Apresentar();
+
+// Pessoa Pessoa_FisicaRepresentacao = new Pessoa();
+
+string apresentacao = "olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.00;
+
+decimal preco = 1.00m;
+
+bool condicao = true;
+
+Console.WriteLine("O valor da variavel quantidade: "+ quantidade);
+Console.WriteLine("O valor da variavel altura "+ altura );
+Console.WriteLine("O valor da variavel preco "+ preco);
+Console.WriteLine("O valor da variavel condicao "+ condicao);
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+DateTime dataAtual2 = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual2);
